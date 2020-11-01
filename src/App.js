e1c90a1d98f38121380400f39b6bd397bc50cd11
shohanur021 <shohanur021@gmail.com>
2020-11-01 16:06:57 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products=[
+    {productName:"Cocacola",price:65},
+    {productName:"Pran-Up",price:50},
+    {productName:"7-Up",price:55},
+    {productName:"Fruto",price:60}
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Product things={products[0]} ></Product>
+      <Product things={products[1]} ></Product>
+      <Product things={products[2]} ></Product>
+      <Product things={products[3]} ></Product>
+      <br></br><br></br><br></br>
+      {
+        products.map(product => <Product things={product}> </Product>)
+      }
+
       </header>
     </div>
   );
+}
+
+function Product(props){
+  const productStyle={
+    border:'2px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:"300px",
+    color:"red",
+    float:"left",
+    margin:"10px"
+  }
+  // console.log(props);
+  const {productName,price}=props.things;    //productName,price must be same as  datails array.
+
+  return(
+    <div style={productStyle}>
+      <h3>Name : {productName}</h3>
+      <h4>{price}$</h4>
+      <button>Buy</button>
+    </div>
+  )
 }
 
 export default App;
